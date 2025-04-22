be86e7a88ea063ddb3fb00a284684ad81e457473
cdryampi <cdryampi@gmail.com>
2025-04-22 10:34:48 +0200

    Fix API_BASE_URL in config.js to include the correct protocol for backend service

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // config.js
 export const API_BASE_URL =
-  "curriculum-backend-edfz.onrender.com";
+  "https://curriculum-backend-edfz.onrender.com";
 export const GLOBAL_DELAY_CALLBACK = 1500;
 export const LOGIN_USER = "yampi";
 export const LOGIN_PASSWORD = "thos";
