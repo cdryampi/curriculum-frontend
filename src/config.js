d74f1e56dae32c8b9f504cf9a77bc3faf02f1869
cdryampi <cdryampi@gmail.com>
2025-04-22 10:27:00 +0200

    Update API_BASE_URL in config.js to point to the correct backend service

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // config.js
 export const API_BASE_URL =
-  "https://curriculum-backend-production.up.railway.app";
+  "curriculum-backend-edfz.onrender.com";
 export const GLOBAL_DELAY_CALLBACK = 1500;
 export const LOGIN_USER = "yampi";
 export const LOGIN_PASSWORD = "thos";
